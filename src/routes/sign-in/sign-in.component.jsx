ceb72708d139dfee7711dffd7074b877e5eced64
= <=>
2023-04-02 21:51:16 +0530

    Added firebase authentication and sign-in component.

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,17 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase.util";
+
+const SignIn = () => {
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    }
+
+    return (
+        <div>
+            <h2>Sign In page</h2>
+            <button onClick={logGoogleUser}>Sign In with google popup</button>
+        </div>
+    );
+}
+
+export default SignIn;
---
+++ src/utils/firebase.util.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfjG7GceRFn3Eas1ay3cdAjqwFG17e1LA",
+  authDomain: "clothing-69887.firebaseapp.com",
+  projectId: "clothing-69887",
+  storageBucket: "clothing-69887.appspot.com",
+  messagingSenderId: "665969887414",
+  appId: "1:665969887414:web:6f3fcd6321e4a1f37cfe17"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  "prompt": "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName: displayName,
+        email: email,
+        createdAt: createdAt
+      });
+    }
+    catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  return userDocRef;
+}
